e77aa9675f423b74236b86e32d6230a1afd31c22
Will-Howton <89426862+Will-Howton@users.noreply.github.com>
2021-08-25 19:00:10 -0500

    Add files via upload

---
+++ base_changer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "Enter your base: ";
+    int base;
+    cin >> base;
+
+    cout << "Enter your number in your base <= 10: ";
+    int num;
+    cin >> num;
+
+    cout << "Enter your destination base: ";
+    int finalBase;
+    cin >> finalBase;
+
+    string finalNum = "";
+
+    while (num > 0) {
+        finalNum = finalNum + to_string(num % base);
+        num /= base;
+    }
+
+    cout << "Your number in base " << base << " is " << finalNum << endl;
+}
